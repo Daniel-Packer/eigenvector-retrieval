4e31c7ea78ebb9ef9773ca329d2f2b6e3ea57a8a
Daniel-Packer <packer.61@osu.edu>
2023-02-07 11:44:33 -0500

    Implemented rudimentary "Hamming Navigation" Idea

--- AlgorithmScripts.py
+++ AlgorithmScripts.py
@@ -11,4 +11,40 @@ class Big_matrix:
   def get_col(self, i):
     return self.__matrix[:, i]
   
+def basis(d : int, i : int):
+  return np.array([1.0 if index==i else 0.0 for index in range(d)])
+  
+# This is one way to generate a problem, Idk if its the best way to make these 
+# problems (our choice of how to do this matters)
+def generate_problem(d: int, k: int):
+  """
+  Returns a eigenvector phase retrieval problem:
+    - a Big_matrix instance
+    - an eigenvalue of the instance
+    - the absolute value of the entries of the eigenvector of that eigenvalue
+  """
+  #d is size of matrix, k is which eigenvector we want from the array of eigenvectors
+  #in our model, this is harder when k is closer to d/2, k = 0 or k = d is easiest
+  assert k < d
+  #not <= bc of 0 indexing
+  rng = np.random.default_rng(seed=1234)
+
+  A = rng.normal(size=(d, d))
+  S = A + A.T
+
+  #S.T = A.T + A = S
+  #this way eigenvalues are real
+  #so now S is symmetric
+
+  big_matrix = Big_matrix(S)
+  w, V = np.linalg.eig(S)
+  v_abs = np.abs(V[:, k])
+  ell = w[k]
+  epsilon = np.sign(V[:, k])
+
+  B = S - ell * np.eye(d)
+
+  return big_matrix, ell, v_abs, epsilon
+
+
   
